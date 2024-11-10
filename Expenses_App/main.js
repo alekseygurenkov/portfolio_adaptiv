488b19d9f40b0c883857af84092073b44e66a7b9
alekseygurenkov <aleksey.sknt@gmail.com>
2024-11-10 21:14:51 +0300

    добавил выпадающий список категорий, в css его настроил и дописал js, пофиксил баги, список работает

--- Expenses_App/main.js
+++ Expenses_App/main.js
@@ -4,9 +4,9 @@ const STATUS_OUT_OF_LIMIT = 'всё плохо';
 
 // Объявление переменных - ссылок на html элементы
 const inputNode = document.getElementById("expenseInput");
+const categorySelectNode = document.getElementById("categorySelect");
 const addButtonNode = document.getElementById("addButton");
 const clearButtonNode = document.getElementById("clearButton");
-
 const totalValueNode = document.getElementById("totalValue");
 const statusNode = document.getElementById("statusText");
 const historyList = document.getElementById("historyList");
@@ -28,69 +28,135 @@ const getTotal = () => {
     expenses.forEach((expense) => {
         // пробегаем по массиву объектов expense, берем из каждого поле amount
         // и прибавляем к переменной sum
-        sum +=expense;
+        sum +=expense.amount;
     }); 
 
     return sum;
 };
 
-// Тут закончил
+// Отрисовывает/обновляет блок с "Всего", "Лимит" и "Статус"
 const renderStatus = () => {
+    // Создаем переменную total(всего) и записываем в неё результат выполнения getTotal
     const total = getTotal(expenses);
     totalValueNode.innerText = total;
 
+    // условие сравнение - что больше "Всего" или "Лимит"
     if (total <= limit) {
+        // Всего меньше чем лимит - все хорошо
         statusNode.innerText = STATUS_IN_LIMIT;
         statusNode.className = "stats__statusText_positive"
     } else {
+        // всего меньше чем лимит - все плохо
+
+        // шаблонная строка - строка в которую можно вставить переменные
+        // тут вставлена переменная STATUS_OUT_OF_LIMIT
+        // и будет вставлено значение разницы между лимитом и общей сумой расходов
         statusNode.innerText = `${STATUS_OUT_OF_LIMIT} (${limit - total} руб)`; // Шаблонная строка
         statusNode.className = "stats__statusText_negative";
     }
 };
 
-const renderHistory = () => {
+// Отрисовывает обновляет блок
+function renderHistory() {
     historyList.innerHTML = "";
-    expenses.forEach((expense) => {
-        const historyItem = document.createElement("li");
-        historyItem.className = "rub";
-        historyItem.innerText = expense;
+    // Сокращенная запись:
+    // expenses.forEach((expense) => {
 
+    // цикл по массиву expenses, где каждый элемент - запись о расходе (сумма и категория)
+    expenses.forEach(function (expense) {
+        // создаем элемент li (он пока создан только в памяти)
+        const historyItem = document.createElement("li");
+
+        // через свойство className также можно прописывать классы
+        historyItem.className = "rub";
+
+        // снова создаем шаблонную строку
+        // формата "категория" - "сумма"  (а не наоборот, чтобы не усложнять html)
+        historyItem.innerText = `${expense.category} - ${expense.amount}`;
+
+        // берем наш li из памяти и вставляем в документ, в конец historyList
         historyList.appendChild(historyItem);
     });
-};
+}
 
-const render = () => {
+// Отрисовывает/обновляет весь интерфейс (в нашем случае - историю, всего, статус)
+function render () {
+    // вызываем функцию обновления статуса и "всего"
     renderStatus();
+
+    // вызываем функцию обновления истории
     renderHistory();
+}
+
+// Возвращает введенную пользователем сумму
+function getExpenseFromUser () {
+    return parseInt(inputNode.value);
+} 
+
+// Возвращает введенную пользователем сумму
+function getSelectedCategory() {
+    return categorySelectNode.value;
+}
+
+// Функция очистки поля ввода суммы
+// на вход получает переменную input, в которой мы ожидаем html элемент input
+
+// альтернативы
+/*function clearInput(input) {
+    input.value = "";
+}*/
+
+const clearInput = function (input) {
+    input.value = "";    
 };
 
-const getExpenseFromUser = () => parseInt(inputNode.value);
+/*
+const clearInput = (input) => {
+    input.value = "";
+}
+ */
 
-const clearInput = () => {
-    inputNode.value = "";
-};
-// Обработчик кнопки длинная запись функцию
+// Функция-обработчик которая будет вызвана при нажатии кнопки Добавить
 function addButtonHandler() {
-    const expense = getExpenseFromUser();
-    if (!expense) {
+    // Сохраняем в переменную currentAmound (текущая сумма) введенную сумму
+    const currentAmound = getExpenseFromUser();
+    if (!currentAmound) {
         return;
     }
 
-    expenses.push(expense);
+    //сохраняем в переменную currentCategory(текущая категория) выбранную категорию
+    const currentCategory = getSelectedCategory();
+    // если текущаяКатегория равна значению Категория
+    if(currentCategory === "Категория") {
+        // тогда выйти из функции, т.к. это значение говорит нам о том
+        // что пользователь не выбрал категорию
+        return;
+    }
 
-    console.log(expenses);
+    // из полученных переменных собираем объект newExpense(новыйРасход)
+    // который состоит из двух полей - amount, в которое записано значение currentAmount
+    // и category, в которое записано значение currentCategory
+    const newExpense = { amount: currentAmound, category: currentCategory };
+    console.log(newExpense);
 
+    // Добавляем наш новыйРасход в массив расходов
+    expenses.push(newExpense);
+
+    // console.log(expenses);
+
+    // перерисовываем интерфейс
     render();
-    clearInput();
-};
-// Обработчик кнопки короткая запись
-const clearButtonHandler = () => {
+
+    // сбрасываем введенную сумму
+    clearInput(inputNode);
+}
+
+// Функция-обработчик кнопки Сбросить расходы
+function clearButtonHandler() {
     expenses = [];
     render();
-};
+}
 
+// ПРивязка функции-обработчика к кнопкам
 addButtonNode.addEventListener("click", addButtonHandler);
 clearButtonNode.addEventListener("click", clearButtonHandler);
-
-
-
